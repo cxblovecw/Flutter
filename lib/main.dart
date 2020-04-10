032b072051da615b297b6531d409ca27be5a9474
cxblovecw <873587091@qq.com>
2020-04-10 15:09:16 +0800

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,46 @@ main(List<String> args) {
 }
 
 class App extends StatelessWidget{
-  List<String> list=List.generate(20, (index){
-    return "$index";
-  });
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'我是标题',
+      title:'GridView',
       home: Scaffold(
         appBar: AppBar(
-          title:Text("使用List创建ListView")
+          title:Text("GridView")
         ),
-        body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (item,index){
-            return ListTile(
-              leading:Icon(Icons.list),
-              title: Text(list[index]),
-            );
-          },
+        // body: GridView.count(
+        //   crossAxisCount: 3,padding: EdgeInsets.all(5),
+        //   mainAxisSpacing: 10,   //主轴间隔
+        //   crossAxisSpacing: 10,  //侧轴间隔
+        //   childAspectRatio: 0.5,
+        //   children: <Widget>[
+        //     Container(width:20,height:20,color:Colors.green),
+        //     Container(width:20,height:20,color:Colors.green),
+        //     Container(width:20,height:20,color:Colors.green),
+        //     Container(width:20,height:20,color:Colors.green),
+        //     Container(width:20,height:20,
+        //   ],
+        //   )
+        // )
+        body: GridView(
+          padding: EdgeInsets.all(20),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 0.8,
+            crossAxisCount: 3
+          ),
+          children: <Widget>[
+            Container(width:20,height:20,color:Colors.green),
+            Container(width:20,height:20,color:Colors.green),
+            Container(width:20,height:20,color:Colors.green),
+            Container(width:20,height:20,color:Colors.green),
+            Container(width:20,height:20,color:Colors.green),
+          ],
         )
-      ),
+        
+      )
     );
 }
 }
