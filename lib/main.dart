ecbd163d5616b19e69692b35fd26e47dcae12662
cxblovecw <873587091@qq.com>
2020-04-10 22:07:21 +0800

    底部导航

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
 import "package:flutter/material.dart";
+import 'package:flutter2/views/category/category.dart';
+import 'package:flutter2/views/home/home.dart';
+import 'package:flutter2/views/profile/profile.dart';
+import 'package:flutter2/views/search/search.dart';
 main(List<String> args) {
   runApp(App());
 }
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return Content();
+  }
+}
 
-class App extends StatelessWidget{
+
+class Content extends State{
+  List<Widget> list=[Home(),Search(),Category(),Profile()];
+  int currentIndex=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       title:'我是标题',
       home: Scaffold(
         appBar: AppBar(
-          title:Text("起步")
+          title:Text("BottomNavigationBar")
         ),
-        body: Center(
-         child:Text('Hello World',style: TextStyle(fontSize:30),)
+        body: list[currentIndex],
+        // 1.创建BottomNavigationBar
+        bottomNavigationBar: BottomNavigationBar(
+          items:[
+            BottomNavigationBarItem(
+              icon:Icon(Icons.home),
+              title: Text("主页")
+            ),
+            BottomNavigationBarItem(
+              icon:Icon(Icons.category),
+              title: Text("分类")
+            ),
+            BottomNavigationBarItem(
+              icon:Icon(Icons.search),
+              title: Text("搜索")
+            ),
+            BottomNavigationBarItem(
+              icon:Icon(Icons.person),
+              title: Text("我的")
+            ),
+          ],
+          // 2.点击设置currentIndex
+          onTap: (index){
+            setState(() {
+            currentIndex=index;
+            });
+          },
+          currentIndex: currentIndex,
         ),
       ),
     );
