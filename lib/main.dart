89038a3ce7ea3c31541c7d99b98d319723da87fb
cxblovecw <873587091@qq.com>
2020-04-10 14:27:39 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,57 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'我是标题',
+      title:'ListView',
       home: Scaffold(
         appBar: AppBar(
-          title:Text("起步")
+          title:Text("ListView")
         ),
         body: Center(
-         child:Text('Hello World',style: TextStyle(fontSize:30),)
+         child:ListView(
+           children: <Widget>[
+             Text("纵向列表"),
+             ListTile(
+               leading: Icon(Icons.perm_camera_mic),
+               title: Text("perm_camera_mic"),
+             ),
+             ListTile(
+               leading: Icon(Icons.person),
+               title: Text("person"),
+             ),
+             ListTile(
+               leading:Icon(Icons.phone),
+               title:Text("phone")
+             ),
+             Container(
+               height: 200,
+               child:ListView(
+                 scrollDirection: Axis.horizontal,
+                 children:<Widget>[
+                   Container(
+                     width: 150,
+                     color:Colors.green,
+                     child: Icon(Icons.phone),
+                   ),
+                  Container(
+                     width: 150,
+                     color:Colors.orange,
+                     child: Icon(Icons.photo_camera),
+                   ),
+                   Container(
+                     width: 150,
+                     color:Colors.yellow,
+                     child:Icon(Icons.not_listed_location)
+                   ),
+                   Container(
+                     width:150,
+                     color:Colors.purple,
+                     child:Icon(Icons.open_with)
+                   ),
+                 ]
+               )
+             )
+           ],
+         )
         ),
       ),
     );
