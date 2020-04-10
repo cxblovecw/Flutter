c87ccf7d2f969ac6a6632857d4978f9bed3aa1dc
cxblovecw <873587091@qq.com>
2020-04-10 17:31:02 +0800

    FatherToSon

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(App());
-
-class App extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title:"导航",
-      home:FirstScreen()
-    );
-  }
+class Product{
+  final String name;
+  final String description;
+  Product(this.name,this.description);
 }
 
+main(List<String> args) {
+  runApp(MaterialApp(
+    title:"导航传递和接收参数",
+    home:FirstScreen(
+      product: List.generate(20, (index){
+        return Product("商品$index", "这是商品$index的描述信息");
+      })),
+    )
+  );
+}
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatelessWidget {
+  final List<Product> product;
+  const FirstScreen({this.product,Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:AppBar(
-          title:Text("导航页面"),
-        ),
-        body:Center(
-          child:RaisedButton(
-            child:Text("前进"),
-            onPressed:(){
-              // 将当前的上下文环境和对应的Widget添加到Navigator中
-              // context为上下文环境
-              Navigator.push(context, 
-              // 这个是页面路由 返回的是需要显示的子页面
-              MaterialPageRoute(
-                builder: (context){
-                  return SecondScreen();
-                }
-              ));
-            }
-          )
-        )
-      );
+      appBar: AppBar(
+        title:Text("导航传递和接收参数")
+      ),
+      body: ListView.builder(
+        itemCount: product.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            leading:Icon(Icons.person),
+            onTap: (){
+              Navigator.push(context,MaterialPageRoute(builder: (context){
+                return Detail(name:product[index].name,description:product[index].description);
+              }));
+            },
+            title:Text("这是第$index个商品")
+          );
+        },
+      ),
+    );   
   }
 }
 
-
-class SecondScreen extends StatelessWidget{
+class Detail extends StatelessWidget{
+  final String name;
+  final String description;
+  Detail({Key key,this.name,this.description});
   @override
   Widget build(BuildContext context) {
+    print(context);
     return Scaffold(
       appBar:AppBar(
-        title:Text("第二个页面"),      
+        title:Text("商品详情页面")
       ),
       body:Center(
-        child: RaisedButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-          child: Text("返回"),
-        )   
+        child:Text("这是商品${name}的详情页面，详情信息是:${description}")
       )
     );
   }
