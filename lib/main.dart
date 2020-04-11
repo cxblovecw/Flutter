e5df9dc0b6544a088354811a77693c7ea73f1580
cxblovecw <873587091@qq.com>
2020-04-11 12:22:21 +0800

    子传父

--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,24 @@ class FirstScreen extends StatelessWidget{
 class JupmButton extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(onPressed: ()async{
-           final result =await Navigator.push(context, MaterialPageRoute(builder: (context){
+    return RaisedButton(onPressed: () async{
+      // 1.不使用async Navigator.push会返回一个Future对象 调用then方法获取传递过来的参数
+          //  final result = Navigator.push(context, MaterialPageRoute(builder: (context){
+          //    return SecondScreen();
+          //  }));
+          //  result.then((value){
+          //   print(value);
+          //      // 这个是使用需要外部的上下文context包含Scaffold
+          //  Scaffold.of(context).showSnackBar(SnackBar(content:Text("$value")));
+          //  });
+      // 2.使用async和await 直接返回结果
+        final result2 =await Navigator.push(context, MaterialPageRoute(builder: (context){
              return SecondScreen();
            }));
-           // 这个是使用需要外部的上下文context包含Scaffold
-           Scaffold.of(context).showSnackBar(SnackBar(content:Text("$result")));
-         },child: Text("前进"),);
+          print(result2);
+               // 这个是使用需要外部的上下文context包含Scaffold
+           Scaffold.of(context).showSnackBar(SnackBar(content:Text("$result2")));
+           },child: Text("前进"));
   }
 }
 class SecondScreen extends StatelessWidget{
@@ -51,7 +62,7 @@ class SecondScreen extends StatelessWidget{
           children:<Widget>[
             RaisedButton(
               onPressed:(){
-                Navigator.pop(context,"button 1");
+                Navigator.pop(context,['name','age','sex']);
               },
               child:Text("按钮一")
             ),
